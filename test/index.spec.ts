5df47f2316cd5ab138c549f507b9f2cabe7580dd
Fernando Almeida <fernando.almeida@gamersclub.com.br>
2022-05-11 16:36:04 -0300

    feat: NEWWWWWW

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,6 +1,6 @@
 import { average } from '../src/index'
 
-describe('when receive a array of valid numbers', () => {
+describe('when receive an array of valid numbers', () => {
   it('should return calculate average with success', async () => {
     const result = average([10, 6, 20])
 
